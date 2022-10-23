f37804948d1c71509b8b8f495e919d80ac04dd2c
Craig Miller <craigmiller160@gmail.com>
2022-10-23 15:53:33 -0400

    Handled tls issue

--- bin/craig-swagger.js
+++ bin/craig-swagger.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const { doGenerateApi } = require('../lib');
 
+// Needed to support locally running self-signed certs
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 if(process.argv.length < 4) {
     throw new Error('Must specify two arguments: the name and url');
 }
