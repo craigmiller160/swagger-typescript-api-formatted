b9dddcc93dc611a813017863a4995a856ed2d9f8
Craig Miller <craigmiller160@gmail.com>
2022-10-23 15:45:13 -0400

    Improved script arguments

--- bin/craig-swagger.js
+++ bin/craig-swagger.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 const { doGenerateApi } = require('../lib');
 
+if(process.argv.length < 4) {
+    throw new Error('Must specify two arguments: the name and url');
+}
+
 doGenerateApi(process.argv[2], process.argv[3]);
